b303eef947d73c54f0dd76a0a3c6fc72b1968868
Zemo <dominik.temiak@student.ssnd.sk>
2026-01-14 11:48:53 +0100

    Fix

--- commit.bat
+++ commit.bat
@@ -1,13 +1,6 @@
 @echo off
-REM -------------------------------------------
-REM Auto Git Add, Commit, and Push Script
-REM -------------------------------------------
 
-REM Set your commit message here
 set /p commitMessage=Enter commit message: 
-
-REM Navigate to your Git repository (optional)
-REM cd "C:\path\to\your\repo"
 
 echo.
 echo Staging all changes...
